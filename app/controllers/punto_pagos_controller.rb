3a0fd4a5d46e69729ca6b7db13607f1150399367
Nicolas <nicolas.floresv@gmail.com>
2014-01-15 12:11:28 -0300

    Punto Pagos
    Comienso de punto pagos, elementos necesarios para trabajar.

---
+++ app/controllers/punto_pagos_controller.rb
@@ -0,0 +1,22 @@
+class PuntoPagosController < ApplicationController
+  def generate_transaction
+  	@booking = Booking.find(params[:booking])
+
+  	# => Datos de la transaccion
+  	trx_id = DateTime.now.to_s.gsub(/[-:T]/i, '') + @booking.id.to_s
+  	amount = @booking.service.price.to_s + '0'
+  	# => Generamos la transaccion
+  	req = PuntoPagos::Request.new()
+  	resp = req.create(trx_id, amount)
+
+  	if resp.success?
+  		redirect_to resp.payment_process_url
+  	end
+  end
+
+  def recive_results
+  	notification = PuntoPagos::Notification.new
+    # This methods requires the headers as a hash and the params object as a hash
+    notification.valid? headers, params
+  end
+end
