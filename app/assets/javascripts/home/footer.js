da9871f0c122c7bce5a81b1b628ce9037f0c94a4
Nicolás <nrossib@gmail.com>
2014-02-12 13:10:59 -0300

    Cambios en el nuevo home, mejoras en el footer y en las fotos Cambios menores en el estilo

--- app/assets/javascripts/home/footer.js
+++ app/assets/javascripts/home/footer.js
@@ -9,7 +9,7 @@ $(function() {
                         '<p>' +
                             '<span class="txt-footer"><i class="fa fa-map-marker"></i> Nuestra Se&ntilde;ora de los Angeles 185, Oficina D, Las Condes, Santiago </span>' +
                             '<span class="txt-footer"><i class="fa fa-phone"></i> (+56 9) 5178 5898 </span>' +
-                            '<span class="txt-footer"><i class="fa fa-envelope"></i> contacto@agendapro.cl </span>' +
+                            '<span><i class="fa fa-envelope"></i> contacto@agendapro.cl </span>' +
                         '</p>' +
                     '</div>' +
                 '</div>' +
