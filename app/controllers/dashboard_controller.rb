0ed20702130dce6c8af29462e7ac019e7127ecd6
Nicolás Rossi <nrossib@gmail.com>
2014-02-24 17:41:37 -0300

    Cambios en el Dashboard para las estadísticas y en el logo del mailer Se agrega un cambio en el seed para que la empresa de test tenga admin

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,9 +5,10 @@ class DashboardController < ApplicationController
   layout "admin"
 
 	def index
-		@lastBookings = Booking.all.order("id desc").limit(5).reverse
-		@todayBookings = Booking.where("DATE(start) = DATE(?)", Time.now)
-		@monthBookings = Booking.where("created_at BETWEEN ? AND ?", Time.now.beginning_of_month, Time.now.end_of_month)
+		# @lastBookings = Booking.all.order("id desc").limit(5).reverse
+		@lastBookings = Booking.where(service_provider_id: ServiceProvider.where(company_id: current_user.company_id)).order("id desc").limit(5).reverse
+		@todayBookings = Booking.where(service_provider_id: ServiceProvider.where(company_id: current_user.company_id)).where("DATE(start) = DATE(?)", Time.now)
+		@monthBookings = Booking.where(service_provider_id: ServiceProvider.where(company_id: current_user.company_id)).where("created_at BETWEEN ? AND ?", Time.now.beginning_of_month, Time.now.end_of_month)
 		@statusArray = []
 		Status.all.each do |status|
 			@statusArray.push([status.name,@monthBookings.where(:status_id => status.id).count]) 
