eaccbe9df7c73b866613ac2480c4e2a5f47e2b48
Nicolas <nicolas.floresv@gmail.com>
2014-03-11 12:25:08 -0300

    Edicion de Usuario
    fixed #44 ahora es posible editar la informacion de usuario.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  #before_filter :configure_permitted_parameters, if: :devise_controller?
 
   before_filter do
     resource = controller_name.singularize.to_sym
@@ -15,16 +14,6 @@ class ApplicationController < ActionController::Base
   end
 
   protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :first_name
-    devise_parameter_sanitizer.for(:sign_up) << :last_name
-    devise_parameter_sanitizer.for(:sign_up) << :email
-    devise_parameter_sanitizer.for(:sign_up) << :phone
-    devise_parameter_sanitizer.for(:sign_up) << :role_id
-    devise_parameter_sanitizer.for(:sign_up) << :company_id
-
-  end
 
   def quick_add
     if current_user && (current_user.role_id != Role.find_by_name("Super Admin").id) && current_user.company_id
