082c47609c5c5cd74e0b64357b2d34ab1f0a2619
shevia <sebastianhevia@gmail.com>
2014-03-11 15:55:19 -0300

    Diseño Formulario servicios

--- app/assets/javascripts/admin/services.js
+++ app/assets/javascripts/admin/services.js
@@ -2,17 +2,37 @@ function categoryChange() {
 	if ($('#categoryCheckboxId').val() == 1) {
 		$('#categoryCheckboxId').val(0);
 		$('#service_service_category_id').prop('disabled', false);
+		$('#service_service_category_id').closest('.form-group').removeClass('hidden');
 		$('#service_service_category_attributes_name').val('');
 		$('#service_service_category_attributes_name').prop('disabled', true);
+		$('#service_service_category_attributes_name').closest('.form-group').addClass('hidden');
 	}
 	else if ($('#categoryCheckboxId').val() == 0) {
 		$('#categoryCheckboxId').val(1);
 		$('#service_service_category_attributes_name').prop('disabled', false);
+		$('#service_service_category_attributes_name').closest('.form-group').removeClass('hidden');
 		$('#service_service_category_id').val('');
 		$('#service_service_category_id').prop('disabled', true);
+		$('#service_service_category_id').closest('.form-group').addClass('hidden');
 	}
 }
 
 $(function() {
 	$('#service_service_category_attributes_name').prop('disabled', true);
+	$('#service_group_service').click(function (e) {
+		serviceGroup();
+	});
+	$('#categoryCheckboxId').click(function (e) {
+		categoryChange();
+	});
 });
+
+function serviceGroup () {
+	if ($('#service_group_service').is(':checked')) {
+		$('#service_capacity').closest('.form-group').removeClass('hidden');
+		$('#foo5').trigger('updateSizes');
+	}
+	else {
+		$('#service_capacity').closest('.form-group').addClass('hidden');
+	}
+}
