7fb4f303cda39f84fc7d4c39121d2c081fc2b755
Nicolas <nicolas.floresv@gmail.com>
2014-03-11 11:07:38 -0300

    Staff Workflow
    fixed #51 ahora solo se muestran los staff del local en el workflow.

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -80,7 +80,7 @@ class ServicesController < ApplicationController
 
   def get_providers
     service = Service.find(params[:id])
-    providers = service.service_providers
+    providers = service.service_providers.where('location_id = ?', params[:local])
     render :json => providers
   end
 
