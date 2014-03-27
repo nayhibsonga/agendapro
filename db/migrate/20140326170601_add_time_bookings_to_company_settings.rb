9d12e8bb7cbd1b0904af0c8a7a154131ddfe1169
Nicolas <nicolas.floresv@gmail.com>
2014-03-27 10:47:35 -0300

    Tiempo bookings y arreglo migracion
    fixed #40 y fixed #41 El workflow refleja los cambios para agendar. Cuanto tiempo antes y cuanto tiempo despues.
    
    Se ajusto un detalle en el migracion, para permitir q funcione con empresas ya creadas

--- db/migrate/20140326170601_add_time_bookings_to_company_settings.rb
+++ db/migrate/20140326170601_add_time_bookings_to_company_settings.rb
@@ -1,6 +1,6 @@
 class AddTimeBookingsToCompanySettings < ActiveRecord::Migration
   def change
-    add_column :company_settings, :before_booking, :integer, null: false
-    add_column :company_settings, :after_booking, :integer, null: false
+    add_column :company_settings, :before_booking, :integer, null: false, default: 24
+    add_column :company_settings, :after_booking, :integer, null: false, default: 6
   end
 end
