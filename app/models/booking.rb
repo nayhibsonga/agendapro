075efcea77811b48d9cc84f9fead962672cfb8af
Nicolas <nicolas.floresv@gmail.com>
2014-04-25 13:54:26 -0300

    Mejoras Calendario Admin - Listo
    ref #128 correccion en el booking reminder

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -102,8 +102,10 @@ class Booking < ActiveRecord::Base
 		@time1 = Time.new.getutc + 1.day
 		@time2 = Time.new.getutc + 2.day
 		where(:start => @time1...@time2).each do |booking|
-			booking.update_column(:status_id, Status.find_by(:name => "Confirmado"))
-			BookingMailer.book_reminder_mail(booking)
+			unless booking.status == Status.find_by(:name => "Cancelado")
+				booking.update_column(:status_id, Status.find_by(:name => "Confirmado")) unless booking.status == Status.find_by(:name => "Pagado")
+				BookingMailer.book_reminder_mail(booking)
+			end
 		end
 	end
 
