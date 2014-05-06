f40edfff7d6791f1a1119433d8d46874a3f0c7ab
shevia <sebastianhevia@gmail.com>
2014-05-06 15:39:33 -0400

    ref #10 Listo para probar gema y claves entregadas por puntopagos.

--- app/controllers/punto_pagos_controller.rb
+++ app/controllers/punto_pagos_controller.rb
@@ -1,14 +1,14 @@
 class PuntoPagosController < ApplicationController
   def generate_transaction
   	trx_id = DateTime.now.to_s.gsub(/[-:T]/i, '')
-  	amount = '10000.00'
+  	amount = 10000.00
     payment_method = '1'
   	req = PuntoPagos::Request.new()
   	resp = req.create(trx_id, amount, payment_method)
 
-    puts resp.inspect
-
   	if resp.success?
+      puts resp.get_error
+      puts resp[:error]
   		redirect_to resp.payment_process_url
     else
       redirect_to punto_pagos_failure_path
