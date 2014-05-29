ab0db3081b4e6b4f86cbc2ee2e30fe06e4f08876
Sebastian Hevia <sebastianhevia@gmail.com>
2014-05-29 12:27:25 -0400

    Hard migrate para bambu.

---
+++ db/migrate/20140527211602_add_client_to_bookings4.rb
@@ -0,0 +1,29 @@
+class AddClientToBookings4 < ActiveRecord::Migration
+  Booking.all.order(:id).limit(500).offset(1500).each do |booking|
+    if booking.email == ''
+      if Client.where(email: '', first_name: booking.first_name, last_name: booking.last_name, company_id: booking.service_provider.company_id).count > 0
+        booking.client = Client.where(email: '', first_name: booking.first_name, last_name: booking.last_name, company_id: booking.service_provider.company_id).first
+        booking.save
+        puts booking.id.to_s + ' encuentra cliente vacio ' + booking.client.id.to_s
+      else
+        c = Client.create(email: '', first_name: booking.first_name, last_name: booking.last_name, phone: booking.phone, company_id: booking.service_provider.company_id)
+        c.save
+        booking.client = c
+        booking.save
+        puts booking.id.to_s + ' crea cliente vacio ' + booking.client.id.to_s
+      end
+    else
+      if Client.where(email: booking.email, company_id: booking.service_provider.company_id).count > 0
+        booking.client = Client.where(email: booking.email, company_id: booking.service_provider.company_id).first
+        booking.save
+        puts booking.id.to_s + ' encuentra cliente lleno ' + booking.client.id.to_s
+      else
+        c = Client.create(email: booking.email, first_name: booking.first_name, last_name: booking.last_name, phone: booking.phone, company_id: booking.service_provider.company_id)
+        c.save
+        booking.client = c
+        booking.save
+        puts booking.id.to_s + ' crea cliente lleno ' + booking.client.id.to_s
+      end
+    end
+  end
+end
