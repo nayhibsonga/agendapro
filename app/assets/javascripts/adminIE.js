5c1f36554425312523c8674fad846a71d2bf579d
Nicolas <nicolas.floresv@gmail.com>
2014-07-15 12:05:21 -0400

    jQuery
    Se actualiza jQuery a la version 1.11.1 y 2.1.1

--- app/assets/javascripts/adminIE.js
+++ app/assets/javascripts/adminIE.js
@@ -1,2 +1,2 @@
-//= require jquery-1.11.0.min
+//= require jquery-1.11.1.min
 //= require jquery-ui-1.10.4.min
--- app/assets/javascripts/adminNIE.js
+++ app/assets/javascripts/adminNIE.js
@@ -1,2 +1,2 @@
-//= require jquery-2.1.0.min
+//= require jquery-2.1.1.min
 //= require jquery-ui-1.10.4.min
