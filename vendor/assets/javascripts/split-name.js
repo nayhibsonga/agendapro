553678b3962ba6653e5e857ece43189887de857c
Nicolas <nicolas.floresv@gmail.com>
2014-07-24 11:23:30 -0400

    Full Name y validacion Workflow
    Se ajustan los formularios para pedir nombre completo, excepto el de clientes.
    Se agrega validacion al workflow.

--- vendor/assets/javascripts/split-name.js
+++ vendor/assets/javascripts/split-name.js
@@ -1,20 +1,32 @@
 /************************************************************************
-*	Description: Take a input that has a string representing a full 	*
+*	Description: Takes a input that has a string representing a full	*
 *	name, and split it into first name and last name. In case that the	*
 *	name has more than 2 words it considered the 3th word as second		*
-*	last name. If it has 4 words it is considered the first as first 	*
-*	name, the second as middle name, and 3th and 4th as last name. In  	*
-*	case of more than 4 words, the first two are first and middle name 	*
+*	last name. If it has 4 words it is considered the first as first	*
+*	name, the second as middle name, and 3th and 4th as last name. In	*
+*	case of more than 4 words, the first two are first and middle name	*
 *	and the rest are last name.											*
 *	Parameter:															*
 *		fullNameOrigin: selector where the value of the full name is.	*
-*		firstNameTarget: selector where the value of the first name 	*
+*		firstNameTarget: selector where the value of the first name		*
 *			should be.													*
-*		lastNameTarget: selector where the value of the last name 		*
+*		lastNameTarget: selector where the value of the last name		*
 *			should be.													*
+*		form: (optional) selector of the form which the inputs belongs	*
 ************************************************************************/
-function split_name (fullNameOrigin, firstNameTarget, lastNameTarget) {
-	$(fullNameOrigin).change(function (event) {
+function split_name (fullNameOrigin, firstNameTarget, lastNameTarget, form) {
+	var fullNameObject, firstNameObject, lastNameObject;
+	if (typeof form == "undefined" && form == null) {
+		fullNameObject = $(fullNameOrigin);
+		firstNameObject = $(firstNameTarget);
+		lastNameObject = $(lastNameTarget);
+	} else{
+		var form = $(form);
+		fullNameObject = form.find(fullNameOrigin);
+		firstNameObject = form.find(firstNameTarget);
+		lastNameObject = form.find(lastNameTarget);
+	};
+	fullNameObject.change(function (event) {
 		var fullName = $(event.target).val();
 		if (typeof fullName !== "undefined" && fullName != null) {
 			if (typeof fullName == 'string') {
@@ -42,8 +54,8 @@ function split_name (fullNameOrigin, firstNameTarget, lastNameTarget) {
 					};
 
 					// Asing the firts and last name
-					$(firstNameTarget).val(firstName);
-					$(lastNameTarget).val(lastName);
+					firstNameObject.val(firstName);
+					lastNameObject.val(lastName);
 				} else{
 					window.console.warn('Origin must be not empty');
 				};
@@ -56,9 +68,30 @@ function split_name (fullNameOrigin, firstNameTarget, lastNameTarget) {
 	});
 }
 
-function compose_name (fullNameTarget, firstNameOrigin, lastNameOrigin) {
-	var firstName = $(firstNameOrigin).val();
-	var lastName = $(lastNameOrigin).val();
+/************************************************************************
+*	Description: Takes two inputs that has the first name and the last	*
+*	name, and generates a full name with the two values.				*
+*	Parameter:															*
+*		fullNameTarget: selector where the value of the full name		*
+*			should be.													*
+*		firstNameTarget: selector where the value of the first name is.	*
+*		lastNameTarget: selector where the value of the last name is.	*
+*		form: (optional) selector of the form which the inputs belongs	*
+************************************************************************/
+function compose_name (fullNameTarget, firstNameOrigin, lastNameOrigin, form) {
+	var fullNameObject, firstNameObject, lastNameObject;
+	if (typeof form == "undefined" && form == null) {
+		fullNameObject = $(fullNameTarget);
+		firstNameObject = $(firstNameOrigin);
+		lastNameObject = $(lastNameOrigin);
+	} else{
+		var form = $(form);
+		fullNameObject = form.find(fullNameTarget);
+		firstNameObject = form.find(firstNameOrigin);
+		lastNameObject = form.find(lastNameOrigin);
+	};
+	var firstName = firstNameObject.val();
+	var lastName = lastNameObject.val();
 	var fullName = firstName + ' ' + lastName;
-	$(fullNameTarget).val(fullName.trim());
+	fullNameObject.val(fullName.trim());
 }
