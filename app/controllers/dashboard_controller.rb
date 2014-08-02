4711aad01f26ae6275b86bba33177aea867c28dc
Nicolas <nicolas.floresv@gmail.com>
2014-08-02 14:51:27 -0400

    Dashboard
    Scroll en la lista de ultimas reservas

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,7 +7,7 @@ class DashboardController < ApplicationController
 	def index
 		# @lastBookings = Booking.all.order("id desc").limit(5).reverse
 		@service_providers = ServiceProvider.where(location_id: Location.where(company_id: current_user.company_id).accessible_by(current_ability))
-		@lastBookings = Booking.where(service_provider_id: @service_providers).order(created_at: :desc).limit(5).reverse
+		@lastBookings = Booking.where(service_provider_id: @service_providers).order(updated_at: :desc).limit(50)
 		@todayBookings = Booking.where(service_provider_id: @service_providers).where("DATE(start) = DATE(?)", Time.now).order(:start)
 		@monthBookings = Booking.where(service_provider_id: @service_providers).where("created_at BETWEEN ? AND ?", Time.now.beginning_of_month, Time.now.end_of_month)
 		@statusArray = []
