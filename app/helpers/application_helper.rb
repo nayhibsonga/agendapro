e6d579b3d5fdf4ea3c553761878a8b22d4e56795
Sebastian Hevia <sebastianhevia@gmail.com>
2014-08-08 18:39:40 -0400

    Version 1 de los reportes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -276,18 +276,74 @@ module ApplicationHelper
 		return top3.merge({ "Otros" => (total - top3_count) })
 	end
 
+	def provider_booking_ranking3(time_range_id, offset, provider_id)
+		if provider_id == 0
+			provider = ServiceProvider.where(active: true)
+			company_id = provider.first.company_id
+		else
+			provider = ServiceProvider.find(provider_id)
+			company_id = provider.company_id
+		end
+		if time_range_id == 0
+			ranking = Booking.where(service_provider_id: provider, location_id: Location.where(company_id: company_id, active: true), service_id: Service.where(company_id: company_id, active: true), start: (1+offset).weeks.ago..offset.weeks.ago).group(:service_id).limit(3).order('count_all desc').count
+		elsif time_range_id == 1
+			ranking = Booking.where(service_provider_id: provider, location_id: Location.where(company_id: company_id, active: true), service_id: Service.where(company_id: company_id, active: true), start: (1+offset).months.ago..offset.months.ago).group(:service_id).limit(3).order('count_all desc').count
+		elsif time_range_id == 2
+			ranking = Booking.where(service_provider_id: provider, location_id: Location.where(company_id: company_id, active: true), service_id: Service.where(company_id: company_id, active: true), start: 2.months.ago..Time.now).group(:service_id).limit(3).order('count_all desc').count
+		elsif time_range_id == 3
+			ranking = Booking.where(service_provider_id: provider, location_id: Location.where(company_id: company_id, active: true), service_id: Service.where(company_id: company_id, active: true), start: 2.months.ago..2.months.from_now).group(:service_id).limit(3).order('count_all desc').count
+		else
+			return 0
+		end
+		top3_count = 0
+		ranking.each do |rank|
+			top3_count += rank[1]
+		end
+		total = provider_booking_count(time_range_id, offset, provider_id)
+		top3 = Hash[ ranking.first(3).map{ |c| [Service.find(c[0]).name,c[1]] } ]
+		return top3.merge({ "Otros" => (total - top3_count) })
+	end
+
+	def location_booking_ranking3(time_range_id, offset, location_id)
+		if location_id == 0
+			location = Location.where(active: true)
+			company_id = location.first.company_id
+		else
+			location = Location.find(location_id)
+			company_id = location.company_id
+		end
+		if time_range_id == 0
+			ranking = Booking.where(service_provider_id: ServiceProvider.where(company_id: company_id, active: true), location_id: location, service_id: Service.where(company_id: company_id, active: true), start: (1+offset).weeks.ago..offset.weeks.ago).group(:service_id).limit(3).order('count_all desc').count
+		elsif time_range_id == 1
+			ranking = Booking.where(service_provider_id: ServiceProvider.where(company_id: company_id, active: true), location_id: location, service_id: Service.where(company_id: company_id, active: true), start: (1+offset).months.ago..offset.months.ago).group(:service_id).limit(3).order('count_all desc').count
+		elsif time_range_id == 2
+			ranking = Booking.where(service_provider_id: ServiceProvider.where(company_id: company_id, active: true), location_id: location, service_id: Service.where(company_id: company_id, active: true), start: 2.months.ago..Time.now).group(:service_id).limit(3).order('count_all desc').count
+		elsif time_range_id == 3
+			ranking = Booking.where(service_provider_id: ServiceProvider.where(company_id: company_id, active: true), location_id: location, service_id: Service.where(company_id: company_id, active: true), start: 2.months.ago..2.months.from_now).group(:service_id).limit(3).order('count_all desc').count
+		else
+			return 0
+		end
+		top3_count = 0
+		ranking.each do |rank|
+			top3_count += rank[1]
+		end
+		total = location_booking_count(time_range_id, offset, location_id)
+		top3 = Hash[ ranking.first(3).map{ |c| [Service.find(c[0]).name,c[1]] } ]
+		return top3.merge({ "Otros" => (total - top3_count) })
+	end
+
 	def status_booking_by_day(time_range_id, offset, status_id, company_id)
 		if status_id == 0
 			status_id = Status.all
 		end
 		if time_range_id == 0
-			by_day = Booking.where(service_provider_id: ServiceProvider.where(company_id: company_id, active: true), location_id: Location.where(company_id: company_id, active: true), service_id: Service.where(company_id: company_id, active: true), start: (1+offset).weeks.ago..offset.weeks.ago).group_by_day_of_week(:start, week_start: 1).count
+			by_day = Booking.where(status_id: status_id, service_provider_id: ServiceProvider.where(company_id: company_id, active: true), location_id: Location.where(company_id: company_id, active: true), service_id: Service.where(company_id: company_id, active: true), start: (1+offset).weeks.ago..offset.weeks.ago).group_by_day_of_week(:start, week_start: 1).count
 		elsif time_range_id == 1
-			by_day = Booking.where(service_provider_id: ServiceProvider.where(company_id: company_id, active: true), location_id: Location.where(company_id: company_id, active: true), service_id: Service.where(company_id: company_id, active: true), start: (1+offset).months.ago..offset.months.ago).group_by_day_of_week(:start, week_start: 1).count
+			by_day = Booking.where(status_id: status_id, service_provider_id: ServiceProvider.where(company_id: company_id, active: true), location_id: Location.where(company_id: company_id, active: true), service_id: Service.where(company_id: company_id, active: true), start: (1+offset).months.ago..offset.months.ago).group_by_day_of_week(:start, week_start: 1).count
 		elsif time_range_id == 2
-			by_day = Booking.where(service_provider_id: ServiceProvider.where(company_id: company_id, active: true), location_id: Location.where(company_id: company_id, active: true), service_id: Service.where(company_id: company_id, active: true), start: 2.months.ago..Time.now).group_by_day_of_week(:start, week_start: 1).count
+			by_day = Booking.where(status_id: status_id, service_provider_id: ServiceProvider.where(company_id: company_id, active: true), location_id: Location.where(company_id: company_id, active: true), service_id: Service.where(company_id: company_id, active: true), start: 2.months.ago..Time.now).group_by_day_of_week(:start, week_start: 1).count
 		elsif time_range_id == 3
-			by_day = Booking.where(service_provider_id: ServiceProvider.where(company_id: company_id, active: true), location_id: Location.where(company_id: company_id, active: true), service_id: Service.where(company_id: company_id, active: true), start: 2.months.ago..2.months.from_now).group_by_day_of_week(:start, week_start: 1).count
+			by_day = Booking.where(status_id: status_id, service_provider_id: ServiceProvider.where(company_id: company_id, active: true), location_id: Location.where(company_id: company_id, active: true), service_id: Service.where(company_id: company_id, active: true), start: 2.months.ago..2.months.from_now).group_by_day_of_week(:start, week_start: 1).count
 		else
 			return 0
 		end
@@ -303,16 +359,47 @@ module ApplicationHelper
 		(0..6).each do |i|
 			day_name = I18n.t(:"date.day_names")[i]
 			if time_range_id == 0
-				booking_count = Booking.where(service_provider_id: ServiceProvider.where(company_id: company_id, active: true), location_id: Location.where(company_id: company_id, active: true), service_id: Service.where(company_id: company_id, active: true), start: (1+offset).weeks.ago..offset.weeks.ago).where('EXTRACT(DOW from ("start"::timestamptz)) = ?', i).group_by_hour_of_day(:start, format: "%l %P").count
+				booking_count = Booking.where(status_id: status_id, service_provider_id: ServiceProvider.where(company_id: company_id, active: true), location_id: Location.where(company_id: company_id, active: true), service_id: Service.where(company_id: company_id, active: true), start: (1+offset).weeks.ago..offset.weeks.ago).where('EXTRACT(DOW from ("start"::timestamptz)) = ?', i).group_by_hour_of_day(:start, format: "%l %P").count
 				by_hour.push( { :name => day_name, :data => booking_count } )
 			elsif time_range_id == 1
-				booking_count = Booking.where(service_provider_id: ServiceProvider.where(company_id: company_id, active: true), location_id: Location.where(company_id: company_id, active: true), service_id: Service.where(company_id: company_id, active: true), start: (1+offset).months.ago..offset.months.ago).where('EXTRACT(DOW from ("start"::timestamptz)) = ?', i).group_by_hour_of_day(:start, format: "%l %P").count
+				booking_count = Booking.where(status_id: status_id, service_provider_id: ServiceProvider.where(company_id: company_id, active: true), location_id: Location.where(company_id: company_id, active: true), service_id: Service.where(company_id: company_id, active: true), start: (1+offset).months.ago..offset.months.ago).where('EXTRACT(DOW from ("start"::timestamptz)) = ?', i).group_by_hour_of_day(:start, format: "%l %P").count
 				by_hour.push( { :name => day_name, :data => booking_count } )
 			elsif time_range_id == 2
-				booking_count = Booking.where(service_provider_id: ServiceProvider.where(company_id: company_id, active: true), location_id: Location.where(company_id: company_id, active: true), service_id: Service.where(company_id: company_id, active: true), start: 2.months.ago..Time.now).where('EXTRACT(DOW from ("start"::timestamptz)) = ?', i).group_by_hour_of_day(:start, format: "%l %P").count
+				booking_count = Booking.where(status_id: status_id, service_provider_id: ServiceProvider.where(company_id: company_id, active: true), location_id: Location.where(company_id: company_id, active: true), service_id: Service.where(company_id: company_id, active: true), start: 2.months.ago..Time.now).where('EXTRACT(DOW from ("start"::timestamptz)) = ?', i).group_by_hour_of_day(:start, format: "%l %P").count
 				by_hour.push( { :name => day_name, :data => booking_count } )
 			elsif time_range_id == 3
-				booking_count = Booking.where(service_provider_id: ServiceProvider.where(company_id: company_id, active: true), location_id: Location.where(company_id: company_id, active: true), service_id: Service.where(company_id: company_id, active: true), start: 2.months.ago..2.months.from_now).where('EXTRACT(DOW from ("start"::timestamptz)) = ?', i).group_by_hour_of_day(:start, format: "%l %P").count
+				booking_count = Booking.where(status_id: status_id, service_provider_id: ServiceProvider.where(company_id: company_id, active: true), location_id: Location.where(company_id: company_id, active: true), service_id: Service.where(company_id: company_id, active: true), start: 2.months.ago..2.months.from_now).where('EXTRACT(DOW from ("start"::timestamptz)) = ?', i).group_by_hour_of_day(:start, format: "%l %P").count
+				by_hour.push( { :name => day_name, :data => booking_count } )
+			else
+				return 0
+			end
+		end
+
+		return by_hour
+	end
+
+	def location_booking_by_hour(time_range_id, offset, location_id)
+		if location_id == 0
+			location = Location.where(active: true)
+			company_id = location.first.company_id
+		else
+			location = Location.find(location_id)
+			company_id = location.company_id
+		end
+		by_hour = []
+		(0..6).each do |i|
+			day_name = I18n.t(:"date.day_names")[i]
+			if time_range_id == 0
+				booking_count = Booking.where(service_provider_id: ServiceProvider.where(company_id: company_id, active: true), location_id: location, service_id: Service.where(company_id: company_id, active: true), start: (1+offset).weeks.ago..offset.weeks.ago).where('EXTRACT(DOW from ("start"::timestamptz)) = ?', i).group_by_hour_of_day(:start, format: "%l %P").count
+				by_hour.push( { :name => day_name, :data => booking_count } )
+			elsif time_range_id == 1
+				booking_count = Booking.where(service_provider_id: ServiceProvider.where(company_id: company_id, active: true), location_id: location, service_id: Service.where(company_id: company_id, active: true), start: (1+offset).months.ago..offset.months.ago).where('EXTRACT(DOW from ("start"::timestamptz)) = ?', i).group_by_hour_of_day(:start, format: "%l %P").count
+				by_hour.push( { :name => day_name, :data => booking_count } )
+			elsif time_range_id == 2
+				booking_count = Booking.where(service_provider_id: ServiceProvider.where(company_id: company_id, active: true), location_id: location, service_id: Service.where(company_id: company_id, active: true), start: 2.months.ago..Time.now).where('EXTRACT(DOW from ("start"::timestamptz)) = ?', i).group_by_hour_of_day(:start, format: "%l %P").count
+				by_hour.push( { :name => day_name, :data => booking_count } )
+			elsif time_range_id == 3
+				booking_count = Booking.where(service_provider_id: ServiceProvider.where(company_id: company_id, active: true), location_id: location, service_id: Service.where(company_id: company_id, active: true), start: 2.months.ago..2.months.from_now).where('EXTRACT(DOW from ("start"::timestamptz)) = ?', i).group_by_hour_of_day(:start, format: "%l %P").count
 				by_hour.push( { :name => day_name, :data => booking_count } )
 			else
 				return 0
