50f7293cdd739c7d85f16dd73599602addef22f6
Sebastian Hevia <sebastianhevia@gmail.com>
2014-08-19 12:19:29 -0400

    Fix pagina de error en sistema de socios.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -238,6 +238,8 @@ class BookingsController < ApplicationController
         else
           flash[:alert] = "No estás ingresado como cliente o no puedes reservas. Porfavor comunícate con la empresa proveedora del servicio."
           @errors = ["No estás ingresado como cliente"]
+          host = request.host_with_port
+          @url = @company.web_address + '.' + host[host.index(request.domain)..host.length]
           render layout: "workflow"
           return
         end
@@ -251,6 +253,8 @@ class BookingsController < ApplicationController
         else
           flash[:alert] = "No estás ingresado como cliente o no puedes reservas. Porfavor comunícate con la empresa proveedora del servicio."
           @errors = ["No estás ingresado como cliente"]
+          host = request.host_with_port
+          @url = @company.web_address + '.' + host[host.index(request.domain)..host.length]
           render layout: "workflow"
           return
         end
