e70e1feaa51eb94ceb9f8aa6f46a837b326d1963
Sebastian Hevia <sebastianhevia@gmail.com>
2014-08-20 16:20:16 -0400

    ref #10 variable mensaje en pagina de exito segun operacion realizada.

--- app/controllers/punto_pagos_controller.rb
+++ app/controllers/punto_pagos_controller.rb
@@ -157,21 +157,18 @@ class PuntoPagosController < ApplicationController
   def success
     if params[:trx_id]
       if BillingLog.find_by_trx_id(params[:trx_id])
-        
+        billing_log = BillingLog.find_by_trx_id(params[:trx_id])
+        @message = "Muchas gracias, has agregado "+billing_log.amount+" meses a tu cuenta AgendaPro."
       elsif PlanLog.find_by_trx_id(params[:trx_id])
-        
+        plan_log = PlanLog.find_by_trx_id(params[:trx_id])
+        @message = "Muchas gracias, has cambiado tu cuenta al plan "+Plan.find(plan_log.new_plan_id).name+" de AgendaPro."
+      else
+        @message = ""
       end
     end
   end
 
   def failure
-    if params[:trx_id]
-      if BillingLog.find_by_trx_id(params[:trx_id])
-        
-      elsif PlanLog.find_by_trx_id(params[:trx_id])
-        
-      end
-    end
   end
 
   def notification
