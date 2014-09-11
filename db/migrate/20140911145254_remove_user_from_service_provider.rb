6ec6a540fe54bb0830e9e8d129ccc7826084ce6a
Sebastian Hevia <sebastianhevia@gmail.com>
2014-09-11 16:36:34 -0300

    Migraciones para nuevos perfiles de usuarios y fix error sin preferencia.

--- db/migrate/20140911145254_remove_user_from_service_provider.rb
+++ db/migrate/20140911145254_remove_user_from_service_provider.rb
@@ -1,9 +1,11 @@
 class RemoveUserFromServiceProvider < ActiveRecord::Migration
   def change
   	ServiceProvider.where("service_providers.user_id IS NOT NULL").each do |service_provider|
-  		user = User.find(service_provider.user_id)
-  		user.service_providers = [service_provider]
-  		user.save
+  		if User.where(id: service_provider.user_id).count > 0
+	  		user = User.find(service_provider.user_id)
+	  		user.service_providers = [service_provider]
+	  		user.save
+	  	end
   	end
     remove_reference :service_providers, :user, index: true
   end
--- db/migrate/20140911145328_remove_location_from_user.rb
+++ db/migrate/20140911145328_remove_location_from_user.rb
@@ -1,8 +1,10 @@
 class RemoveLocationFromUser < ActiveRecord::Migration
   def change
   	User.where("users.location_id IS NOT NULL").each do |user|
-  		user.locations = Location.where(id: user.location_id)
-  		user.save
+  		if Location.where(id: user.location_id).count > 0
+	  		user.locations = Location.where(id: user.location_id)
+	  		user.save
+	  	end
   	end
     remove_reference :users, :location, index: true
   end
