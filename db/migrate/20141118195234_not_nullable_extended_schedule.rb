f34a92d96c5392cd7bcc6daf3d001f02454ea32c
shevia <sebastianhevia@gmail.com>
2014-11-19 17:57:43 -0300

    ref #297 Fix migracion.

--- db/migrate/20141118195234_not_nullable_extended_schedule.rb
+++ db/migrate/20141118195234_not_nullable_extended_schedule.rb
@@ -5,21 +5,23 @@ class NotNullableExtendedSchedule < ActiveRecord::Migration
   	change_column :company_settings, :extended_max_hour, :time, :default => "20:00", :null => false
   end
   CompanySetting.all.each do |company_setting|
-  	location_ids = company_setting.company.locations.pluck(:id)
-	if LocationTime.where(location_id: location_ids).count > 0
-		changed = false
-		first_open_time = LocationTime.where(location_id: location_ids).order(:open).first.open
-		last_close_time = LocationTime.where(location_id: location_ids).order(:close).last.close
-		if company_setting.extended_min_hour > first_open_time
-			company_setting.extended_min_hour = first_open_time
-			changed = true
-		end
-		if company_setting.extended_max_hour < last_close_time
-			company_setting.extended_max_hour = last_close_time
-			changed = true
-		end
-		if changed
-			company_setting.save!
+  	if company_setting.company
+	  	location_ids = company_setting.company.locations.pluck(:id)
+		if LocationTime.where(location_id: location_ids).count > 0
+			changed = false
+			first_open_time = LocationTime.where(location_id: location_ids).order(:open).first.open
+			last_close_time = LocationTime.where(location_id: location_ids).order(:close).last.close
+			if company_setting.extended_min_hour > first_open_time
+				company_setting.extended_min_hour = first_open_time
+				changed = true
+			end
+			if company_setting.extended_max_hour < last_close_time
+				company_setting.extended_max_hour = last_close_time
+				changed = true
+			end
+			if changed
+				company_setting.save!
+			end
 		end
 	end
   end
