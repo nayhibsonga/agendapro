1d6dd6c4b681a0c7a51edf3f8350360d6058d89d
shevia <sebastianhevia@gmail.com>
2014-12-19 17:29:15 -0300

    ref #310 comentario metodo modelo locales, servicios categorizados.

--- app/models/location.rb
+++ app/models/location.rb
@@ -124,6 +124,48 @@ class Location < ActiveRecord::Base
 
 	def categories
 
+		# def location_categorized_services
+	 #    location_resources = Location.find(params[:location]).resource_locations.pluck(:resource_id)
+	 #    service_providers = ServiceProvider.where(location_id: params[:location])
+
+	 #    categories = ServiceCategory.where(:company_id => Location.find(params[:location]).company_id).order(order: :asc)
+	 #    services = Service.where(:active => true, :id => ServiceStaff.where(service_provider_id: service_providers.pluck(:id)).pluck(:service_id)).order(order: :asc)
+	 #    service_resources_unavailable = ServiceResource.where(service_id: services)
+	 #    if location_resources.any?
+	 #      if location_resources.length > 1
+	 #        service_resources_unavailable = service_resources_unavailable.where('resource_id NOT IN (?)', location_resources)
+	 #      else
+	 #        service_resources_unavailable = service_resources_unavailable.where('resource_id <> ?', location_resources)
+	 #      end
+	 #    end
+	 #    if service_resources_unavailable.any?
+	 #      if service_resources_unavailable.length > 1
+	 #        services = services.where('services.id NOT IN (?)', service_resources_unavailable.pluck(:service_id))
+	 #      else
+	 #        services = services.where('id <> ?', service_resources_unavailable.pluck(:service_id))
+	 #      end
+	 #    end
+
+	 #    categorized_services = Array.new
+	 #    categories.each do |category|
+	 #      services_array = Array.new
+	 #      services.each do |service|
+	 #        if service.service_category_id == category.id
+	 #          serviceJSON = service.attributes.merge({'name_with_small_outcall' => service.name_with_small_outcall })
+	 #          services_array.push(serviceJSON)
+	 #        end
+	 #      end
+	 #      service_hash = {
+	 #        :id => category.id,
+	 #        :category => category.name,
+	 #        :services => services_array
+	 #      }
+	 #      categorized_services.push(service_hash)
+	 #    end
+
+	 #    render :json => categorized_services
+	 #  end
+
 
 	    service_providers = self.service_providers
 
