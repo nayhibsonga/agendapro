21ac2d6ee112f6c254d079bcd48dd00a462e201c
Ignacio Gómez <iegomez@uc.cl>
2015-01-06 12:21:37 -0300

    #Ref326 Fin fallo de pago.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141231155216) do
+ActiveRecord::Schema.define(version: 20150106151442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -310,6 +310,7 @@ ActiveRecord::Schema.define(version: 20141231155216) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "transfer_complete",           default: false
+    t.boolean  "canceled",                    default: false
   end
 
   create_table "payment_statuses", force: true do |t|
