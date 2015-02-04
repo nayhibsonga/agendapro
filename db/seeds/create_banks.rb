47ef2b7b5defd66cfb9faea3f8a59cb26f4ee7b6
Ignacio Gómez <iegomez@uc.cl>
2015-02-04 11:14:01 -0300

    Creación de bancos.

--- db/seeds/create_banks.rb
+++ db/seeds/create_banks.rb
@@ -1,17 +1,17 @@
-banks = [[code: 1, name = "Banco Chile - CityBank"],
-[code: 12, name = "Banco Del Estado"],
-[code: 14, name = "Banco Scotia Sud Americano"],
-[code: 27, name = "Corpbanca"],
-[code: 28, name = "Banco Bice"],
-[code: 37, name = "Banco Santander Santiago"],
-[code: 39, name = "Banco Itau"],
-[code: 16, name = "Banco Credito"],
-[code: 46, name = "ABM Amor Bank (Chile)"],
-[code: 49, name = "Banco Security"],
-[code: 51, name = "Banco Falabella"],
-[code: 504, name = "Banco Bilbao Vizcaya Argentaria, Chile"],
-[code: 507, name = "Banco del Desarrollo"],
-[code: 0, name = "Otro"]
+banks = [[code: 1, name: "Banco Chile - CityBank"],
+[code: 12, name: "Banco Del Estado"],
+[code: 14, name: "Banco Scotia Sud Americano"],
+[code: 27, name: "Corpbanca"],
+[code: 28, name: "Banco Bice"],
+[code: 37, name: "Banco Santander Santiago"],
+[code: 39, name: "Banco Itau"],
+[code: 16, name: "Banco Credito"],
+[code: 46, name: "ABM Amor Bank (Chile)"],
+[code: 49, name: "Banco Security"],
+[code: 51, name: "Banco Falabella"],
+[code: 504, name: "Banco Bilbao Vizcaya Argentaria, Chile"],
+[code: 507, name: "Banco del Desarrollo"],
+[code: 0, name: "Otro"]
 ]
 
 banks.each do |arr|
