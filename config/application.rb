bd26029ae76b7cf91c350b3140ce63c580da58d4
shevia <sebastianhevia@gmail.com>
2015-02-13 14:46:28 -0300

    Prueba cambio en rails path para capistrano.

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module Agendapro
     # config.assets.precompile += %w( ckeditor/* )
 
     config.before_configuration do
-      env_file = '/config/local_env.yml'
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
       YAML.load(File.open(env_file)).each do |key, value|
         ENV[key.to_s] = value
       end if File.exists?(env_file)
