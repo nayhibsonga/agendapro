895ae9c22d90cbc66ee59f627becd9ca59150a85
Ignacio Gómez <iegomez@uc.cl>
2015-02-20 04:33:13 -0300

    Creación de online_cancelation_policy.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,6 +6,15 @@ class DashboardController < ApplicationController
 
 	def index
 		# @lastBookings = Booking.all.order("id desc").limit(5).reverse
+
+		if current_user.company.company_setting.online_cancelation_policy.nil?
+			ocp = OnlineCancelationPolicy.new
+			ocp.company_setting_id = current_user.company.company_setting.id
+			ocp.save
+			current_user.company.company_setting.online_cancelation_policy = ocp
+			current_user.company.company_setting.save
+		end
+
 		@due_payment = true if Company.find(current_user.company_id).payment_status == PaymentStatus.find_by_name("Emitido") unless current_user.role_id == Role.find_by_name("Super Admin").id
 		@service_providers = ServiceProvider.where(location_id: Location.where(company_id: current_user.company_id).accessible_by(current_ability))
 		@lastBookings = Booking.where(service_provider_id: @service_providers).where('start >= ?', Time.now).order(updated_at: :desc).limit(50)
