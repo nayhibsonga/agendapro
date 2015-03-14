6e2add1f5382a69b1279eeec1f2cf84f9faec25c
shevia <sebastianhevia@gmail.com>
2015-03-13 21:30:08 -0300

    Fix error de mergeo.

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20150223150625) do
-=======
 ActiveRecord::Schema.define(version: 20150225140427) do
->>>>>>> c51fdfdbcbab5c4277b8f3e82e315f06c8665831
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
