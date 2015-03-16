f0a6f28d48f1eb1518489155b140067d8f51bc0b
shevia <sebastianhevia@gmail.com>
2015-03-16 07:52:46 -0300

    Cambios finales para subir.

--- db/schema.rb
+++ db/schema.rb
@@ -107,10 +107,10 @@ ActiveRecord::Schema.define(version: 20150312160347) do
     t.float    "price",               default: 0.0
     t.boolean  "provider_lock",       default: false
     t.integer  "max_changes",         default: 2
+    t.integer  "deal_id"
     t.boolean  "payed",               default: false
     t.string   "trx_id",              default: ""
     t.string   "token",               default: ""
-    t.integer  "deal_id"
     t.integer  "booking_group"
     t.integer  "payed_booking_id"
   end
@@ -233,7 +233,7 @@ ActiveRecord::Schema.define(version: 20150312160347) do
     t.boolean  "schedule_overcapacity",       default: true,                  null: false
     t.boolean  "provider_overcapacity",       default: true,                  null: false
     t.boolean  "resource_overcapacity",       default: true,                  null: false
-    t.integer  "booking_confirmation_time",   default: 1,                     null: false\
+    t.integer  "booking_confirmation_time",   default: 1,                     null: false
     t.boolean  "booking_history",             default: false
     t.boolean  "staff_code",                  default: false
     t.integer  "booking_configuration_email", default: 0
@@ -608,11 +608,8 @@ ActiveRecord::Schema.define(version: 20150312160347) do
     t.integer  "order",                       default: 0
     t.integer  "block_length",                default: 30
     t.integer  "booking_configuration_email", default: 0
-<<<<<<< HEAD
     t.decimal  "comission_value",             default: 0.0,  null: false
     t.integer  "comission_option",            default: 0,    null: false
-=======
->>>>>>> 567d9050d43dfc2cd8975211f6becc0ef83acbce
   end
 
   add_index "service_providers", ["company_id"], name: "index_service_providers_on_company_id", using: :btree
