f171901a85e6ae2cf734f6a487d9de5615459568
Ignacio Gómez <iegomez@uc.cl>
2015-03-24 13:55:29 -0300

    Elegir recibir ofertas al correo.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,12 +6,12 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
     def sign_up_params
-      allow = [:first_name, :last_name, :phone, :email, :password, :password_confirmation, :role_id, company_attributes: [:name, :web_address, :plan_id, :logo, :payment_status_id, :economic_sector_id, company_setting_attributes: [:before_booking, :after_booking]]]
+      allow = [:first_name, :last_name, :phone, :email, :password, :password_confirmation, :role_id, :receives_offers, company_attributes: [:name, :web_address, :plan_id, :logo, :payment_status_id, :economic_sector_id, company_setting_attributes: [:before_booking, :after_booking]]]
       params.require(resource_name).permit(allow)
     end
 
     def account_update_params
-    	allow = [:first_name, :last_name, :phone, :email, :password, :password_confirmation, :current_password, :role_id, company_attributes: [:name, :web_address, :plan_id, :logo, :payment_status_id, :economic_sector_id]]
+    	allow = [:first_name, :last_name, :phone, :email, :password, :password_confirmation, :current_password, :role_id, :receives_offers, company_attributes: [:name, :web_address, :plan_id, :logo, :payment_status_id, :economic_sector_id]]
     	params.require(resource_name).permit(allow)
     end
 
