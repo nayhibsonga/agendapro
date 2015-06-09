cb38389f81334d8df17b1d397e51ae2505e259e7
Nicolas Flores <nicolas.floresv@gmail.com>
2015-06-09 12:01:52 -0300

    Notification center:  * migracion para convertir lista

--- db/migrate/20150526210816_convert_old_notification_to_new.rb
+++ db/migrate/20150526210816_convert_old_notification_to_new.rb
@@ -1,31 +1,37 @@
 class ConvertOldNotificationToNew < ActiveRecord::Migration
   def change
     # Notificaciones por local
-    Location.where(notification: true).where.not(bokking_configuration_email: 3) do |local|
+    Location.where(notification: true, active: true).where.not(booking_configuration_email: 3) do |local|
       # Tipo de configuracion
       conf = local.booking_configuration_email
       if conf == 2
         conf = local.company.company_setting.booking_configuration_email
       end
 
+      # Notificacion correspondiente al local de la compañia, con unico email
+      notification = NotificationEmail.where(company: local.company, email: local.email, receptor_type: 1)
+
       case conf
         when 0 # Transactional
           # Notification email
-          notification = NotificationEmail.create(
-              company: local.company,
-              email: local.email,
-              receptor_type: 2,
-              summary: false,
-              new: true,
-              modified: true,
-              confirmed: true,
-              canceled: true,
-              new_web: true,
-              modified_web: true,
-              confirmed_web: true,
-              canceled_web: true
-            )
-          notification.save
+          notification = notification.where(summary: false).first
+          unless notification
+            notification = NotificationEmail.create(
+                company: local.company,
+                email: local.email,
+                receptor_type: 1,
+                summary: false,
+                new: true,
+                modified: true,
+                confirmed: true,
+                canceled: true,
+                new_web: true,
+                modified_web: true,
+                confirmed_web: true,
+                canceled_web: true
+              )
+            notification.save
+          end
 
           # Notification locations
           nLocation = NotificationLocation.create(
@@ -35,12 +41,15 @@ class ConvertOldNotificationToNew < ActiveRecord::Migration
           nLocation.save
         when 1 # Summary
           # Notification email
-          notification = NotificationEmail.create(
-              company: local.company,
-              email: local.email,
-              receptor_type: 2
-            )
-          notification.save
+          notificacion = notificacion.where(summary: true).first
+          unless notification
+            notification = NotificationEmail.create(
+                company: local.company,
+                email: local.email,
+                receptor_type: 1
+              )
+            notification.save
+          end
 
           # Notification locations
           nLocation = NotificationLocation.create(
@@ -49,15 +58,73 @@ class ConvertOldNotificationToNew < ActiveRecord::Migration
             )
           nLocation.save
         else # No notification
-          puts "The local {local.name} has set to no notification"
+          puts "The local " + local.name + " has set to no notification"
       end
     end
 
     # Notificacion por prestador
-    Revisar los prestadores por local
-    verificar si el email del prestador ya existe en la Notificacion
-      Si existe, verificar si es tipo local o tipo prestador
-      si es tipo prestador, incluir el prestador en la lista de prestadores
-    Si no existe el email, generar la notificacion
+    ServiceProvider.where(active: true, location_id: Location.where(active: true)).where.not(booking_configuration_email: 3).order(:location_id) do |provider|
+      # Tipo de configuracion
+      conf = provider.booking_configuration_email
+      if conf == 2
+        conf = provider.location.booking_configuration_email
+        if conf == 2
+          conf = provider.company.company_setting.booking_configuration_email
+        end
+      end
+
+      # Notificacion correspondiente a prestadores de la compañia, con unico email
+      notification = NotificationEmail.where(company: provider.company, email: provider.notification_email, receptor_type: 2)
+
+      case conf
+        when 0 # Transactional
+          # Notification email
+          notification = notification.where(summary: false).first
+          unless notification
+            notification = NotificationEmail.create(
+                company: provider.company,
+                email: provider.notification_email,
+                receptor_type: 2,
+                summary: false,
+                new: true,
+                modified: true,
+                confirmed: true,
+                canceled: true,
+                new_web: true,
+                modified_web: true,
+                confirmed_web: true,
+                canceled_web: true
+              )
+            notification.save
+          end
+
+          # Notification provider
+          nProvider = NotificationProvider.create(
+              service_provider: provider,
+              notification_email: notificacion
+            )
+          nProvider.save
+        when 1 # Summary
+          # Notification email
+          notification = notification.where(summary: true).first
+          unless notification
+            notification = NotificationEmail.create(
+                company: local.company,
+                email: local.email,
+                receptor_type: 2
+              )
+            notification.save
+          end
+
+          # Notification provider
+          nProvider = NotificationProvider.create(
+              service_provider: provider,
+              notification_email: notificacion
+            )
+          nProvider.save
+        else
+          puts "Provider " + provider.public_name + " has set to no notification"
+      end
+    end
   end
 end
