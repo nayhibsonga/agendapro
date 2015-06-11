39df1151efd6215b864f8a097eccd5d6cb171885
Ignacio Gómez <iegomez@uc.cl>
2015-06-11 12:00:24 -0400

    Validación de horas.

--- app/assets/javascripts/validations/admin/company-edit.js
+++ app/assets/javascripts/validations/admin/company-edit.js
@@ -134,8 +134,7 @@ $.validator.addMethod('night', function(value, element, params) {
 $(function() {
 	$('#promo-times-form').validate({
 		errorPlacement: function(error, element) {
-			var id = element.attr('id');
-			error.appendTo(element.parent().next());
+			error.appendTo(element.next());
 		},
 		rules: {
 			'company_setting[promo_time_attributes][morning_start(4i)]': {
@@ -188,13 +187,16 @@ $(function() {
 			}
 		},
 		highlight: function(element) {
-			$(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+			console.log($(element));
+			$(element).parent().removeClass('has-success').addClass('has-error');
 			$(element).parent().children('.form-control-feedback').removeClass('fa fa-check').addClass('fa fa-times');
 		},
 		success: function(element) {
-			$(element).closest('.form-group').removeClass('has-error').addClass('has-success');
-			$(element).parent().parent().children('.form-control-feedback').removeClass('fa fa-times').addClass('fa fa-check');
-			$(element).parent().empty()
+			console.log(element.parent());
+			$(element).closest('promo-hour-select').removeClass('has-error').addClass('has-success');
+			$(element).parent().removeClass('has-error').addClass('has-success');
+			$(element).parent().children('.form-control-feedback').removeClass('fa fa-times').addClass('fa fa-check');
+			//$(element).parent().empty()
 		},
 		submitHandler: function(form) {
 			form.submit();
