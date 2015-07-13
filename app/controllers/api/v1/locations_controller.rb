24ada3d477e09e19fe6a9db3186315f7e4672b35
shevia <sebastianhevia@gmail.com>
2015-07-13 14:31:57 -0300

    Primer metodo para API App Movil.

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
   	class LocationsController < ApiController
       def index
-      	@locations = Location.where(active: true, online_booking: true).limit(20)
+      	@locations = Location.where(active: true, online_booking: true).last(20)
       end
   	end
   end
