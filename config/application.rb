1b9e3de0ddade9af3387a8325b8dbd6eeb091ca6
shevia <sebastianhevia@gmail.com>
2015-07-17 11:43:31 -0300

    Fallbacks manuales para i18n.

--- config/application.rb
+++ config/application.rb
@@ -52,7 +52,9 @@ module Agendapro
     # config.time_zone = 'Santiago'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('locales', '*.{rb,yml}').to_s]
+    config.i18n.fallbacks = true
+    config.i18n.load_path += Dir[Rails.root.join('config','locales', '*.{rb,yml}').to_s]
+    config.i18n.fallbacks = {"es_CL" => "es"}
     config.i18n.default_locale = :"es"
   end
 end
