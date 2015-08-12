91bc3cd020de8414d4e79283620396a561ccac85
Nicolas Flores <nicolas.floresv@gmail.com>
2015-08-12 16:02:39 -0300

    Condicion para que no se caiga en caso de no encontrar el logo

--- db/migrate/20150731191617_recreate_company_logo.rb
+++ db/migrate/20150731191617_recreate_company_logo.rb
@@ -1,7 +1,7 @@
 class RecreateCompanyLogo < ActiveRecord::Migration
   def change
     Company.all.each do |company|
-      if !company.logo.url.include? 'logo_vacio'
+      if (!company.logo.url.include? 'logo_vacio') && File.exist?('public' + company.logo.url)
         company.logo.recreate_versions!
       end
     end
