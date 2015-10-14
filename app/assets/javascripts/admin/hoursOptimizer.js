1f35e3f22b3c8915795e5c2aa54fa7088c2a1973
Nicolas Flores <nicolas.floresv@gmail.com>
2015-10-14 16:27:59 -0300

    Optimizador de hora: cambio de texto del boton en el ultimo paso

--- app/assets/javascripts/admin/hoursOptimizer.js
+++ app/assets/javascripts/admin/hoursOptimizer.js
@@ -30,6 +30,7 @@ function loadServiceModal () {
     loadService();
   }); // Bind click event
 
+  $('#nextButton').text('Siguiente');
   $('#nextButton').off('click'); // Unbind click event
   $('#nextButton').click(function (e) {
     loadHourModal();
@@ -215,6 +216,7 @@ function loadUserModal () {
   $('#hoursOptimizer #new_booking').show();
   $('#optimizerTitle').html(userTitle);
 
+  $('#nextButton').text('Reservar');
   $('#nextButton').off('click'); // Unbind click event
   $('#nextButton').click(function (e) {
     if ($('#hoursOptimizer #new_booking').valid()) {
