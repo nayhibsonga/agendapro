a6857349e39e5034e29af49ea3259cdeab4a34b8
shevia <sebastianhevia@gmail.com>
2015-11-12 10:27:31 -0300

    fix planes nuevos.

--- app/models/plan_country.rb
+++ app/models/plan_country.rb
@@ -2,7 +2,7 @@ class PlanCountry < ActiveRecord::Base
   belongs_to :plan
   belongs_to :country
 
-  validates :plan, :country, :price, :presence => true
+  validates :country, :price, :presence => true
 
   validates_uniqueness_of :plan_id, scope: :country_id
 end
