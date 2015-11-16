9cb32a57d5d0c24b119a87af59fd5008b0bd5e93
shevia <sebastianhevia@gmail.com>
2015-11-15 22:16:13 -0300

    fix assets.

--- app/assets/javascripts/angular.js
+++ app/assets/javascripts/angular.js
@@ -1,16 +1,16 @@
-//= require ./angular/libs/javascripts/jquery-1.11.3.min
-//= require ./angular/libs/javascripts/angular.min
-//= require ./angular/libs/javascripts/angular-resource.min
-//= require ./angular/libs/javascripts/angular-route.min
-//= require ./angular/libs/javascripts/angular-cookies.min
-//= require ./angular/libs/javascripts/angular-animate.min
-//= require ./angular/libs/javascripts/google-maps/lodash.min
-//= require ./angular/libs/javascripts/google-maps/index
-//= require ./angular/libs/javascripts/google-maps/angular-google-maps.min
-//= require ./angular/libs/javascripts/google-maps/ngAutocomplete
-//= require ./angular/libs/javascripts/jquery-ui.min
+//= require angular/libs/javascripts/jquery-1.11.3.min
+//= require angular/libs/javascripts/angular.min
+//= require angular/libs/javascripts/angular-resource.min
+//= require angular/libs/javascripts/angular-route.min
+//= require angular/libs/javascripts/angular-cookies.min
+//= require angular/libs/javascripts/angular-animate.min
+//= require angular/libs/javascripts/google-maps/lodash.min
+//= require angular/libs/javascripts/google-maps/index
+//= require angular/libs/javascripts/google-maps/angular-google-maps.min
+//= require angular/libs/javascripts/google-maps/ngAutocomplete
+//= require angular/libs/javascripts/jquery-ui.min
 //= require app
-//= require_tree ./angular
+//= require_tree angular
 
 // Re organize Tmpls on complete App separation
 
