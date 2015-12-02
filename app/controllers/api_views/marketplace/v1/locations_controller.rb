b4b39a74d1c9b8fc59167aeb70f892a9f747391a
shevia <sebastianhevia@gmail.com>
2015-12-02 19:14:12 -0300

    Fix unowned companies.

--- app/controllers/api_views/marketplace/v1/locations_controller.rb
+++ app/controllers/api_views/marketplace/v1/locations_controller.rb
@@ -217,7 +217,7 @@ module ApiViews
 					results = ActiveRecord::Base.connection.execute(query)
 
 					results.each do |result|
-						if Location.find(result['location_id']) && Location.find(result['location_id']).owned
+						if Location.find(result['location_id']) && Location.find(result['location_id']).company.owned
 							@results << Location.find(result['location_id'])
 						end
 					end
