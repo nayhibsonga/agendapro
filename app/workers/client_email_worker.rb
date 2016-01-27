0eb3ec8f018ba11316cf8a5b7e11d2cf4036803b
Ignacio Diez <ig.diez10@gmail.com>
2016-01-27 19:46:05 -0300

    Added email filtering

--- app/workers/client_email_worker.rb
+++ app/workers/client_email_worker.rb
@@ -3,8 +3,8 @@ class ClientEmailWorker
   def self.perform(sending)
     content = Email::Content.find(sending.sendable_id)
     company = content.company
-    recipients = content.to.split(', ').uniq
-    if company.reached_mailing_limit? && recipients > company.mails_left
+    recipients = filter_mails(content.to.split(', ').uniq)
+    if company.reached_mailing_limit? && recipients.size > company.mails_left
       sending.update(status: 'canceled', detail: "Reached monthly limit sending #{recipients.size} mails. (#{company.settings.monthly_mails}/#{company.plan.monthly_mails})")
     else
       total_sendings = 0
@@ -18,6 +18,14 @@ class ClientEmailWorker
       sending.update(status: 'delivered', sent_date: DateTime.now, total_sendings: total_sendings, total_recipients: total_recipients)
       company.settings.update(monthly_mails: company.settings.monthly_mails + total_recipients)
     end
+  end
+
+  def self.filter_mails(recipients)
+    filtered = []
+    recipients.each do |mail|
+      filtered << mail if mail =~ /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+    end
+    filtered
   end
 
 end
