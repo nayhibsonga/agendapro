cd412809b4460ecd1c16fd44efba1243c1859c05
shevia <sebastianhevia@gmail.com>
2016-03-10 14:09:42 -0300

    Fix migración.

--- db/migrate/20160217160504_transform_location_districts_to_outcall_places.rb
+++ db/migrate/20160217160504_transform_location_districts_to_outcall_places.rb
@@ -2,7 +2,7 @@ class TransformLocationDistrictsToOutcallPlaces < ActiveRecord::Migration
   def change
     Location.all.each do |location|
       outcall_places = ""
-      location.districts.each do |district|
+      District.where(id: LocationOutcallDistrict.where(location_id: location.id).pluck(:district_id)).each do |district|
         outcall_places << "#{district.name}\r\n"
       end
       location.update_column(:outcall_places, outcall_places.strip)
