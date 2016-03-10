8f0fc3a1523c281e7dc0b8911357447ac5a40bc6
shevia <sebastianhevia@gmail.com>
2016-03-10 14:15:32 -0300

    Fix migración.

--- db/migrate/20160224183013_transform_location_district_to_country.rb
+++ db/migrate/20160224183013_transform_location_district_to_country.rb
@@ -1,10 +1,10 @@
 class TransformLocationDistrictToCountry < ActiveRecord::Migration
   def change
     Location.all.each do |location|
-      district = local.district_id && District.find(local.district_id) ? District.find(local.district_id) : nil
-      city = district.present? && City.find(District.find(local.district_id).city_id) ? City.find(District.find(local.district_id).city_id) : nil
-      region = city.present? && Region.find(City.find(District.find(local.district_id).city_id).region_id) ? Region.find(City.find(District.find(local.district_id).city_id).region_id) : nil
-      country = region.present? && Country.find(Region.find(City.find(District.find(local.district_id).city_id).region_id).country_id) ? Country.find(Region.find(City.find(District.find(local.district_id).city_id).region_id).country_id) : nil
+      district = location.district_id && District.find(location.district_id) ? District.find(location.district_id) : nil
+      city = district.present? && City.find(District.find(location.district_id).city_id) ? City.find(District.find(location.district_id).city_id) : nil
+      region = city.present? && Region.find(City.find(District.find(location.district_id).city_id).region_id) ? Region.find(City.find(District.find(location.district_id).city_id).region_id) : nil
+      country = region.present? && Country.find(Region.find(City.find(District.find(location.district_id).city_id).region_id).country_id) ? Country.find(Region.find(City.find(District.find(location.district_id).city_id).region_id).country_id) : nil
       location.update(country_id: country.id)
     end
   end
