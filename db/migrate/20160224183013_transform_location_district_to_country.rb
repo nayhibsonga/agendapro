d805ada872ea50c2a310b426c481b37bc9bf6b9d
shevia <sebastianhevia@gmail.com>
2016-03-18 08:32:26 -0300

    Fix migración.

--- db/migrate/20160224183013_transform_location_district_to_country.rb
+++ db/migrate/20160224183013_transform_location_district_to_country.rb
@@ -4,7 +4,7 @@ class TransformLocationDistrictToCountry < ActiveRecord::Migration
       district = location.district_id && District.find(location.district_id) ? District.find(location.district_id) : nil
       city = district.present? && City.find(District.find(location.district_id).city_id) ? City.find(District.find(location.district_id).city_id) : nil
       region = city.present? && Region.find(City.find(District.find(location.district_id).city_id).region_id) ? Region.find(City.find(District.find(location.district_id).city_id).region_id) : nil
-      country = region.present? && Country.find(Region.find(City.find(District.find(location.district_id).city_id).region_id).country_id) ? Country.find(Region.find(City.find(District.find(location.district_id).city_id).region_id).country_id) : nil
+      country = region.present? && Country.find(Region.find(City.find(District.find(location.district_id).city_id).region_id).country_id) ? Country.find(Region.find(City.find(District.find(location.district_id).city_id).region_id).country_id) : location.company.country
       location.update(country_id: country.id)
     end
   end
