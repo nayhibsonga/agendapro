1235b5d680af88e09be42de11d03e4cb1f064e3f
shevia <sebastianhevia@gmail.com>
2016-04-25 23:52:36 -0300

    Client mailer.

--- lib/mail_interceptor.rb
+++ lib/mail_interceptor.rb
@@ -6,7 +6,7 @@ class MailInterceptor
       message.bcc = ""
     end
 
-    if message.to.empty? && message.cc.nil? && message.bcc.nil? && message.smtp_envelope_to.nil?
+    if message.to.nil? && message.cc.nil? && message.bcc.nil? && message.smtp_envelope_to.nil?
       message.perform_deliveries = false
       puts "Blocked email"
     end
