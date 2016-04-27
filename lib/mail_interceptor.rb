81ada83f3bcca8c1db93f4f2f135acbb4ca21fbc
shevia <sebastianhevia@gmail.com>
2016-04-27 13:26:48 -0300

    Blacklist email.

--- lib/mail_interceptor.rb
+++ lib/mail_interceptor.rb
@@ -1,6 +1,6 @@
 class MailInterceptor
   def self.delivering_email(message)
-    if Rails.env != 'production'
+    unless Rails.env == 'production'
       message.subject = "[#{message.to}] #{message.subject}"
       message.to = ENV['EMAIL_FILTER'] || message.to
       message.bcc = ""
@@ -8,7 +8,7 @@ class MailInterceptor
 
     if message.to.nil? && message.cc.nil? && message.bcc.nil? && message.smtp_envelope_to.nil?
       message.perform_deliveries = false
-      puts "Blocked email"
+      puts "Blocked email #{message.subject}"
     end
   end
 end
