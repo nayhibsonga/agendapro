f254f683f268be34678ffa868902836efd48b49c
shevia <sebastianhevia@gmail.com>
2016-05-13 14:24:06 -0300

    Parser de eventos sparkpost.

--- app/models/sparkpost_email_log.rb
+++ app/models/sparkpost_email_log.rb
@@ -22,20 +22,20 @@ class SparkpostEmailLog < ActiveRecord::Base
             log.assign_attributes(status: message_event["type"], email: message_event["rcpt_to"], timestamp: DateTime.strptime("1318996912",'%s'))
           end
         elsif track_event.present?
-          if message_event["rcpt_meta"]["booking_ids"].present?
-            message_event["rcpt_meta"]["booking_ids"].each do |booking_id|
-              log = BookingEmailLog.find_by(transmission_id: message_event["transmission_id"], booking_id: booking_id)
-              if log.present? && message_event["type"] == "open"
+          if track_event["rcpt_meta"]["booking_ids"].present?
+            track_event["rcpt_meta"]["booking_ids"].each do |booking_id|
+              log = BookingEmailLog.find_by(transmission_id: track_event["transmission_id"], booking_id: booking_id)
+              if log.present? && track_event["type"] == "open"
                 log.update(opens: log.opens + 1)
-              elsif log.present? && message_event["type"] == "click"
+              elsif log.present? && track_event["type"] == "click"
                 log.update(clicks: log.clicks + 1)
               end
             end
-          elsif message_event["rcpt_meta"]["campaign_id"].present? && Email::Content.find_by(id: message_event["rcpt_meta"]["campaign_id"]) && Client.find_by(email: message_event["rcpt_to"], company_id: Email::Content.find_by(id: message_event["rcpt_meta"]["campaign_id"]).company.id)
-            log = ClientEmailLog.find_by(transmission_id: message_event["transmission_id"], campaign_id: message_event["rcpt_meta"]["campaign_id"], client_id: Client.find_by(email: message_event["rcpt_to"], company_id: Email::Content.find_by(id: message_event["rcpt_meta"]["campaign_id"]).company.id).id)
-            if log.present? && message_event["type"] == "open"
+          elsif track_event["rcpt_meta"]["campaign_id"].present? && Email::Content.find_by(id: track_event["rcpt_meta"]["campaign_id"]) && Client.find_by(email: track_event["rcpt_to"], company_id: Email::Content.find_by(id: track_event["rcpt_meta"]["campaign_id"]).company.id)
+            log = ClientEmailLog.find_by(transmission_id: track_event["transmission_id"], campaign_id: track_event["rcpt_meta"]["campaign_id"], client_id: Client.find_by(email: track_event["rcpt_to"], company_id: Email::Content.find_by(id: track_event["rcpt_meta"]["campaign_id"]).company.id).id)
+            if log.present? && track_event["type"] == "open"
               log.update(opens: log.opens + 1)
-            elsif log.present? && message_event["type"] == "click"
+            elsif log.present? && track_event["type"] == "click"
               log.update(clicks: log.clicks + 1)
             end
           end
