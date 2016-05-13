dfe98a46b525a99caee9b41f0f6c1f4c63abcf72
shevia <sebastianhevia@gmail.com>
2016-05-13 15:41:34 -0300

    Parser de eventos sparkpost.

--- app/models/sparkpost_email_log.rb
+++ app/models/sparkpost_email_log.rb
@@ -15,12 +15,12 @@ class SparkpostEmailLog < ActiveRecord::Base
           if message_event["rcpt_meta"]["booking_ids"].present?
             message_event["rcpt_meta"]["booking_ids"].each do |booking_id|
               log = BookingEmailLog.find_or_initialize_by(transmission_id: message_event["transmission_id"], booking_id: booking_id)
-              log.assign_attributes(status: message_event["type"], email: message_event["rcpt_to"], timestamp: DateTime.strptime("1318996912",'%s'))
+              log.assign_attributes(status: message_event["type"], recipient: message_event["rcpt_to"], timestamp: DateTime.strptime("1318996912",'%s'))
               log.save
             end
           elsif message_event["rcpt_meta"]["campaign_id"].present? && Email::Content.find_by(id: message_event["rcpt_meta"]["campaign_id"]) && Client.find_by(email: message_event["rcpt_to"], company_id: Email::Content.find_by(id: message_event["rcpt_meta"]["campaign_id"]).company.id)
             log = ClientEmailLog.find_or_initialize_by(transmission_id: message_event["transmission_id"], campaign_id: message_event["rcpt_meta"]["campaign_id"], client_id: Client.find_by(email: message_event["rcpt_to"], company_id: Email::Content.find_by(id: message_event["rcpt_meta"]["campaign_id"]).company.id).id)
-            log.assign_attributes(status: message_event["type"], email: message_event["rcpt_to"], timestamp: DateTime.strptime("1318996912",'%s'))
+            log.assign_attributes(status: message_event["type"], recipient: message_event["rcpt_to"], timestamp: DateTime.strptime("1318996912",'%s'))
             log.save
           end
         elsif track_event.present?
