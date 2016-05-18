08321c98214cf5df2bbe58b93ee01bd9de5ceaa4
Ignacio Gómez <iegomez@uc.cl>
2016-05-18 14:04:07 -0400

    Fixes varios.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3885,193 +3885,64 @@ class BookingsController < ApplicationController
     end
 
     if mobile_request?
-      @service = @booking.service
-      @provider = @booking.service_provider
-      @location = @booking.location
-      cancelled_id = Status.find_by(name: 'Cancelado').id
+      
+      parser = PostgresParser.new
 
+      @service = @booking.service
+      service_duration = @service.duration
       # Data
       @date = Date.parse(@booking.start.to_s)
       puts params[:datepicker]
       if !params[:datepicker].blank?
         @date = Date.parse(params[:datepicker])
       end
-      service_duration = @service.duration
-      company_setting = @company.company_setting
-      provider_breaks = ProviderBreak.where(:service_provider_id => @provider.id)
-
-      @available_time = Array.new
-
-      # Variable Data
-      provider_times = @provider.provider_times.where(day_id: @date.cwday).order(:open)
-      bookings = @provider.bookings.where(:start => @date.to_time.beginning_of_day..@date.to_time.end_of_day).order(:start)
-
-      # Data
-      provider = @booking.service_provider
-      provider_breaks = provider.provider_breaks
-      provider_times_first = provider.provider_times.order(:open).first
-      provider_times_final = provider.provider_times.order(close: :desc).first
-
-      # Block Hour
-      # {
-      #   status: 'available/occupied/empty/past',
-      #   hour: {
-      #     start: '10:00',
-      #     end: '10:30',
-      #     provider: ''
-      #   }
-      # }
-
-      @available_time = Array.new
-
-      # Variable Data
       day = @date.cwday
-      provider_times = provider.provider_times.where(day_id: day).order(:open)
+      @location = @location = @booking.location
+      company_setting = CompanySetting.find(Company.find(@location.company_id).company_setting)
+      company_id = company_setting.company_id
+      cancelled_id = Status.find_by(name: 'Cancelado').id
+      location_times_first = @location.location_times.order(:open).first
+      location_times_final = @location.location_times.order(close: :desc).first
+      location_times = @location.location_times.where(day_id: day).order(:open)
+      location_times_first_open = location_times_first.open
+      location_times_final_close = location_times_final.close
 
-      if provider_times.length > 0
+      providers = []
 
-      provider_times_first_open = provider_times_first.open
-      provider_times_final_close = provider_times_final.close
+      providers = ServiceProvider.where(id: @booking.service_provider_id)
 
-      provider_times_first_open_start = provider_times_first_open
+      @available_time = []
 
-      time_offset = 0
+      if location_times.length > 0
 
-      while (provider_times_first_open_start <=> provider_times_final_close) < 0 do
+        start_date = DateTime.new(@date.year, @date.month, @date.day, location_times_first_open.hour, location_times_first_open.min)
+        end_date = DateTime.new(@date.year, @date.month, @date.day, location_times_final_close.hour, location_times_final_close.min)
 
-        provider_times_first_open_end = provider_times_first_open_start + service_duration.minutes
+        ActiveRecord::Base.connection.execute("SELECT * FROM available_hours(#{company_id}, #{@location.id}, ARRAY#{[@booking.service_provider_id]}, ARRAY#{[@service.id]}, ARRAY#{[0]}, '#{start_date}', '#{end_date}', false, ARRAY#{providers.pluck(:id)})").each do |row|
 
-        status = 'empty'
-        hour = {
-          :start => '',
-          :end => ''
-        }
+            db_hours = parser.parse_pg_array(row["hour_array"])
 
-        available_provider = ''
-        provider_time_valid = false
-        provider_free = true
-        provider_times.each do |provider_time|
-          if (provider_time.open - provider_times_first_open_end)*(provider_times_first_open_start - provider_time.close) > 0
-            if provider_time.open > provider_times_first_open_start
-              time_offset += (provider_time.open - provider_times_first_open_start)/1.minutes
-              if time_offset < service_duration
-                provider_times_first_open_start = provider_time.open
-                provider_times_first_open_end = provider_times_first_open_start + service_duration.minutes
-              end
-            end
-            if provider_time.open <= provider_times_first_open_start && provider_time.close >= provider_times_first_open_end
-              provider_time_valid = true
-            elsif provider_time.open <= provider_times_first_open_start
-              time_offset = time_offset % service_duration
-              provider_times_first_open_start -= time_offset.minutes
-              provider_times_first_open_end -= time_offset.minutes
-              time_offset = 0
-            else
-              provider_times_first_open_start -= time_offset.minutes
-              provider_times_first_open_end -= time_offset.minutes
-              time_offset = 0
-            end
+            db_hours.each_with_index do |db_hour, index|
+                  pg_hour = parser.parse_pg_array(db_hour)
+
+                  hour = {
+                    :start => pg_hour[0].to_datetime.strftime("%R"),
+                    :end => pg_hour[1].to_datetime.strftime("%R")
+                  }
+
+                  block_hour = Hash.new
+                  block_hour[:date] = @date
+                  block_hour[:hour] = hour
+                  block_hour[:service_provider_id] = pg_hour[2].to_i
+
+                  @available_time << block_hour
+
+                end
+
           end
-          break if provider_time_valid
-        end
-
-        open_hour = provider_times_first_open_start.hour
-        open_min = provider_times_first_open_start.min
-        start_block = (open_hour < 10 ? '0' : '') + open_hour.to_s + ':' + (open_min < 10 ? '0' : '') + open_min.to_s
-
-        next_open_hour = provider_times_first_open_end.hour
-        next_open_min = provider_times_first_open_end.min
-        end_block = (next_open_hour < 10 ? '0' : '') + next_open_hour.to_s + ':' + (next_open_min < 10 ? '0' : '') + next_open_min.to_s
-
-
-        start_time_block = DateTime.new(@date.year, @date.mon, @date.mday, open_hour, open_min)
-        end_time_block = DateTime.new(@date.year, @date.mon, @date.mday, next_open_hour, next_open_min)
-        now = DateTime.new(DateTime.now.year, DateTime.now.mon, DateTime.now.mday, DateTime.now.hour, DateTime.now.min)
-        before_now = start_time_block - company_setting.before_booking / 24.0
-        after_now = now + company_setting.after_booking * 30
-
-        if provider_time_valid
-          if (before_now <=> now) < 1
-            status = 'past'
-          elsif (after_now <=> end_time_block) < 1
-            status = 'past'
-          else
-            status = 'occupied'
-            Booking.where(:service_provider_id => provider.id, :start => @date.to_time.beginning_of_day..@date.to_time.end_of_day).each do |provider_booking|
-              unless provider_booking.status_id == cancelled_id
-                if (provider_booking.start.to_datetime - end_time_block) * (start_time_block - provider_booking.end.to_datetime) > 0
-                  if !@service.group_service || @service.id != provider_booking.service_id
-                    if !provider_booking.is_session || (provider_booking.is_session and provider_booking.is_session_booked)
-                      provider_free = false
-                      break
-                    end
-                  elsif @service.group_service && @service.id == provider_booking.service_id && provider.bookings.where(:service_id => @service.id, :start => start_time_block).where.not(status_id: Status.find_by_name('Cancelado')).count >= @service.capacity
-                    if !provider_booking.is_session || (provider_booking.is_session and provider_booking.is_session_booked)
-                      provider_free = false
-                      break
-                    end
-                  end
-                end
-              end
-            end
-            if @service.resources.count > 0
-              @service.resources.each do |resource|
-                if !@location.resource_locations.pluck(:resource_id).include?(resource.id)
-                  provider_free = false
-                  break
-                end
-                used_resource = 0
-                group_services = []
-                @location.bookings.where(:start => @date.to_time.beginning_of_day..@date.to_time.end_of_day).each do |location_booking|
-                  if location_booking.status_id != cancelled_id && (location_booking.start.to_datetime - end_time_block) * (start_time_block - location_booking.end.to_datetime) > 0
-                    if location_booking.service.resources.include?(resource)
-                      if !location_booking.service.group_service
-                        used_resource += 1
-                      else
-                        if location_booking.service != @service || location_booking.service_provider != provider
-                          group_services.push(location_booking.service_provider.id)
-                        end
-                      end
-                    end
-                  end
-                end
-                if group_services.uniq.count + used_resource >= ResourceLocation.where(resource_id: resource.id, location_id: @location.id).first.quantity
-                  provider_free = false
-                  break
-                end
-              end
-            end
-            ProviderBreak.where(:service_provider_id => provider.id).order(:start).each do |provider_break|
-              if (provider_break.start.to_datetime - end_time_block)*(start_time_block - provider_break.end.to_datetime) > 0
-                provider_free = false
-              end
-              break if !provider_free
-            end
-
-            if provider_free
-              status = 'available'
-              available_provider = provider.id
-            end
-          end
-        end
-
-        if ['past','available','occupied'].include? status
-          hour = {
-            :start => start_block,
-            :end => end_block
-          }
-        end
-
-        block_hour = Hash.new
-
-        block_hour[:date] = @date
-        block_hour[:hour] = hour
-        block_hour[:provider] = available_provider
-
-        @available_time << block_hour if status == 'available'
-        provider_times_first_open_start = provider_times_first_open_end
       end
-    end
+
+      @available_time
 
     end
 
@@ -4115,7 +3986,7 @@ class BookingsController < ApplicationController
       BookingHistory.create(booking_id: @booking.id, action: "Modificada por Cliente", start: @booking.start, status_id: @booking.status_id, service_id: @booking.service_id, service_provider_id: @booking.service_provider_id, user_id: user, notes: @booking.notes, company_comment: @booking.company_comment)
     else
       #flash[:error] = "Hubo un error actualizando tu reserva. Inténtalo nuevamente."
-      @errors = @booking.errors
+      @errors = @booking.errors.full_messages
     end
 
     # => Domain parser
@@ -4449,8 +4320,8 @@ class BookingsController < ApplicationController
         current_user ? user = current_user.id : user = 0
         BookingHistory.create(booking_id: @booking.id, action: "Cancelada por Cliente", start: @booking.start, status_id: @booking.status_id, service_id: @booking.service_id, service_provider_id: @booking.service_provider_id, user_id: user, notes: @booking.notes, company_comment: @booking.company_comment)
       else
-        flash[:error] = "Hubo un error cancelando tu reserva. Inténtalo nuevamente."
-        @errors = @booking.errors
+        #flash[:error] = "Hubo un error cancelando tu reserva. Inténtalo nuevamente."
+        @errors = @booking.errors[:clients]
       end
 
     end
@@ -7417,6 +7288,7 @@ class BookingsController < ApplicationController
       logger.debug "SELECT * FROM available_hours(#{company_id}, #{local.id}, ARRAY#{providers_arr}, ARRAY#{services_arr}, ARRAY#{bundles_arr}, '#{start_date}', '#{end_date}', false, ARRAY#{first_providers_ids})"
 
       ActiveRecord::Base.connection.execute("SELECT * FROM available_hours(#{company_id}, #{local.id}, ARRAY#{providers_arr}, ARRAY#{services_arr}, ARRAY#{bundles_arr}, '#{start_date}', '#{end_date}', false, ARRAY#{first_providers_ids})").each do |row|
+        logger.debug row.inspect
         db_hours = parser.parse_pg_array(row["hour_array"])
         db_gap = row["positive_gap"].to_i
         day_positive_gaps[day - 1] = db_gap
