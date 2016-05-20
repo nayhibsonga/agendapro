f9242df079def577d6d161df73eaa8506df1eb25
Ignacio Gómez <iegomez@uc.cl>
2016-05-20 11:51:29 -0400

    Fix de day en las funciones de postgres.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
 
-	layout "home" #:layout
+  layout "home" #:layout
 
-	include UrlHelper
+  include UrlHelper
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
