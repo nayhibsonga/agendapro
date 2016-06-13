06f49c267850cdb6a15b7bc4311af791172e1d3e
shevia <sebastianhevia@gmail.com>
2016-06-13 12:00:34 -0400

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160608171000) do
+ActiveRecord::Schema.define(version: 20160609114120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -539,6 +539,9 @@ ActiveRecord::Schema.define(version: 20160608171000) do
     t.integer  "booking_leap",                default: 15
     t.boolean  "allows_overlap_hours",        default: false
     t.boolean  "require_cashier_code",        default: true
+    t.string   "color_light"
+    t.string   "color_normal"
+    t.string   "color_dark"
   end
 
   add_index "company_settings", ["company_id"], name: "index_company_settings_on_company_id", using: :btree
