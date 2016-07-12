dc87b34cd14ef6d6425abeae8cc4502f63087931
shevia <sebastianhevia@gmail.com>
2016-07-12 12:00:15 -0400

    Fix para procesar emails aparte.

--- config/schedule.rb
+++ config/schedule.rb
@@ -70,6 +70,10 @@ every 1.days, :at => '0:15 am' do
   runner 'PettyCash.close_on_schedule'
 end
 
+every 1.minutes do
+  runner 'SparkpostEmailLog.update_logs'
+end
+
 environment = ENV['RAILS_ENV'] || 'production'
 every :reboot do
     command "cd #{path} && #{environment_variable}=#{environment} bin/delayed_job -n4 restart"
